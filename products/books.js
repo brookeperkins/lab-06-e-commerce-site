9ae733572c05c414785caea70738c5b836b30c76
brookeperkins <brookemmperkins@gmail.com>
2020-07-13 21:32:00 -0700

    forloop

--- products/books.js
+++ products/books.js
@@ -1,2 +1,11 @@
 // for loop goes here
+import books from '../data/books-list.js';
+import { renderBooks } from './render-books.js';
 
+const list = document.getElementById('books');
+
+for (let i = 0; i < books.length; i++) {
+    const book = books[i];
+    const dom = renderBooks(book);
+    list.appendChild(dom);
+}
