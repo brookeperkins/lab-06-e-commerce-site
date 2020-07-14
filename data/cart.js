666a8ddf90882c654cc5e0f9efd48be434751d55
brookeperkins <brookemmperkins@gmail.com>
2020-07-14 15:45:34 -0700

    designed cart in HTML

---
+++ data/cart.js
@@ -0,0 +1,21 @@
+//hard code cart items to populate cart
+//does 'quantity' translate? it is suggesting 'number'
+
+const cart = [{
+    id: 'book1'
+    quantity: 1
+}, {
+    id: 'book5'
+    quantity: 3
+}, {
+    id: 'book2'
+    quantity: 2
+}, {
+    id: 'book4'
+    quantity: 1
+}, {
+    id: 'book3'
+    quantity: 4
+}];
+
+export default cart;
